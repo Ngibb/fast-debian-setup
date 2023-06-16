00c4e1c1e119a5a56717ae529220331c4e4dca88
Nolan Gibb <ngibb94@gmail.com>
2023-06-15 21:20:06 -0700

    Create workstation-setup.sh

---
+++ workstation-setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get -yq install aptitude
+sudo aptitude -yq install vim zsh curl git
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/Ngibb/nolan-omzsh-theme.git
+ln -s  ~/nolan-omzsh-theme/.nolan.zsh-theme ~/.oh-my-zsh/themes/ngibb.zsh-theme
