5020715b223b6d9fb355e5546c900e3d56e80b7b
ngibb <ngibb94@gmail.com>
2023-06-17 12:42:59 -0700

    feat: vim as default git editor

--- workstation-setup.sh
+++ workstation-setup.sh
@@ -5,3 +5,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 git clone https://github.com/Ngibb/nolan-omzsh-theme.git
 ln -s  ~/nolan-omzsh-theme/.nolan.zsh-theme ~/.oh-my-zsh/themes/ngibb.zsh-theme
+
+
+mkdir -p ~/git
+git config --global core.editor vim
